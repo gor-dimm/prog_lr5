5fea4920a3b6677dc57ec8358c879d0e7d0cdd02
gor-dimm <booo130502@gmail.com>
2021-12-17 21:51:22 +0300

    -

--- task3.py
+++ task3.py
@@ -12,9 +12,9 @@ def mark(student, **scores):
 if __name__ == '__main__':
     mark(
         "Горлов Дмитрий",
-        Jurisprudence="Хорошо", Information_Technology_in_Telecommunications="Отлично",
+        Jurisprudence="Хорошо", Information_Technologies_in_Telecommunications="Отлично",
         Physical_Basics_of_Telecommunications="Хорошо", Philosophy="Отлично", Russian_language="Отлично",
         English_language="Хорошо", Russian_History="Отлично", Physical_Education="Отлично",
         Elective_Classes_of_PE="Отлично", Introduction_to_Web_Technologies="Зачтено",
-        Crossplatform_Programming_Basics="Зачтено"
+        Basics_of_Crossplatform_Programming="Зачтено"
     )
